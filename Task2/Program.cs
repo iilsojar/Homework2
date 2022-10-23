3f926bf792a38d6e4ada417550c654aa8635befa
Svetlana <iilsojar@gmail.com>
2022-10-23 15:23:57 +0200

    исправила опечатки

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -6,14 +6,14 @@ int numberA = int.Parse(Console.ReadLine());
 Console.WriteLine("введите второе число: ");
 int numberB = int.Parse(Console.ReadLine());
 if (numberA == numberB * numberB)
-    Console.WriteLine ($"число {numberA} является квадартом числа {numberB}");
+    Console.WriteLine ($"число {numberA} является квадратом числа {numberB}");
 else
 {
-    Console.WriteLine ($"число {numberA} не является квадартом числа {numberB}");
+    Console.WriteLine ($"число {numberA} не является квадратом числа {numberB}");
 }
 if (numberB == numberA * numberA)
-Console.WriteLine ($"число {numberB} является квадартом числа {numberA}");
+Console.WriteLine ($"число {numberB} является квадратом числа {numberA}");
 else
 {
-    Console.WriteLine ($"число {numberB} не является квадартом числа {numberA}");
+    Console.WriteLine ($"число {numberB} не является квадратом числа {numberA}");
 }
