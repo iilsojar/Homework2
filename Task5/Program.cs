8573bee79f738ad6cba67593b37ffdde267e0615
Svetlana <iilsojar@gmail.com>
2022-10-23 17:33:22 +0200

    Найти расстояние между точками в пространстве 3D

---
+++ Task5/Program.cs
@@ -0,0 +1,24 @@
+﻿// Найти расстояние между точками в пространстве 3D
+
+Console.Clear();
+Console.WriteLine("введите координаты xa: ");
+double xa = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("введите координаты xb: ");
+double xb = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("введите координаты ya: ");
+double ya = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("введите координаты yb: ");
+double yb = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("введите координаты ca: ");
+double ca = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("введите координаты cb: ");
+double cb = Convert.ToDouble(Console.ReadLine());
+
+double distance = Math.Sqrt(Math.Pow((xb - xa),2) + Math.Pow((yb - ya), 2) + Math.Pow((cb - ca), 2));
+Console.WriteLine($"расстояние между тремя точками на плоскости {distance}");
+
